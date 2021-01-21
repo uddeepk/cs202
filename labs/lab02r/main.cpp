2e7d04478b6a8955bfbfb69aec539cc2fec017bd
uddeepk <uddeepk@gmail.com>
2021-01-21 11:24:37 -0900

    Lab02r reformat output to easier track prog

--- labs/lab02r/main.cpp
+++ labs/lab02r/main.cpp
@@ -11,20 +11,24 @@ int main() {
     cout << "\n";
     Person p3 ( "Andy"); //Calls other constructor
     cout << "\n";
+    cout << "** Passing by value:";
     passByValue(p3);
     cout << "\n";
+    cout << "**Pass by ref: ";
     displayNameFiveTimes(p3);
     cout << "\n";
     Person p4 ( "Steve");
     cout << "\n";
+    cout << "**Pass by const ref";
     Person p5 = combineTwoNames(p4, p3);
     cout << "\n";
     std::vector<std::string> testNames {"name1", "name2", "name3", "name4", "name5"};
     std::vector <Person> vecPer;
+    cout << "Pushing into vector" << std::endl;
     for(const auto &str : testNames) {
         vecPer.push_back(Person(str));
     }
     cout << "\n";
-
+    cout << "End of code " << std::endl;
     return 0;
 }
