45361122275c57909e6f9241f61f980cf696843b
uddeepk <uddeepk@gmail.com>
2021-01-26 11:07:42 -0900

    create raw ptr to dynamically allocated Person

--- labs/lab03/main.cpp
+++ labs/lab03/main.cpp
@@ -2,9 +2,10 @@
 #include <iostream>
 #include <vector>
 
-using std::cout;
+using std::cout; using std::endl;
 
 int main() {
+    /*
     int x;
     int y;
     int *xPtr = &x;
@@ -22,34 +23,39 @@ int main() {
     int * aPtr = array;
     ++aPtr;
     cout << *aPtr << "\n";
-
+    */
     ////////////////////////////////////////////////////////////////////////////////////
     // Lab 02 work
 
     //Write a program that uses your class from Lab 2 that reports when it is created, copied,
     // and destroyed. In this program you should
 
-    //Create a raw pointer to a dynamically allocated object of your class. Is this object ever
+    // 1.Create a raw pointer to a dynamically allocated object of your class. Is this object ever
     // destroyed?
-    //
-    // 1. Create a unique_ptr to an object of your class. Note when the object is created, and
+
+    Person * per1 = new Person( "Bart");
+
+    cout << per1->getName() << endl;
+    // The object is not destroyed. Noticed no destructor called.
+
+    // 2. Create a unique_ptr to an object of your class. Note when the object is created, and
     // that it is eventually destroyed. This object should be created with your
     // "other constructor," which takes a parameter.
     //
-    // 2.Transfer ownership of that object to a different unique_ptr. Note that the object itself
+    // 3.Transfer ownership of that object to a different unique_ptr. Note that the object itself
     // is not copied.
     //
-    // 3.Demonstrate the calling of a member function of your object through the unique_ptr.
+    // 4.Demonstrate the calling of a member function of your object through the unique_ptr.
     //
-    // 4.Make a shared_ptr to a dynamically allocated object of your class.
+    // 5.Make a shared_ptr to a dynamically allocated object of your class.
     //
-    // 5.Make another shared_ptr that points at the same object. Note that the object does not
+    // 6.Make another shared_ptr that points at the same object. Note that the object does not
     // get destroyed until both shared_ptrs go out of scope.
     //
-    // 6.Before each step, print a line to the console explaining what you're doing,
-    // like "Creating raw pointer.", "Creating unique_ptr.", "Transferring ownership to
-    // new unique_ptr.", "Calling a member function.", "Creating shared_ptr.",
-    // "Creating another shared_ptr to the same object."
+    //Before each step, print a line to the console explaining what you're doing,
+    //like "Creating raw pointer.", "Creating unique_ptr.", "Transferring ownership to
+    //new unique_ptr.", "Calling a member function.", "Creating shared_ptr.",
+    //"Creating another shared_ptr to the same object."
 
     //Be sure to use make_shared and make_unique, do not call the constructors for shared_ptr
     // and unique_ptr directly.
