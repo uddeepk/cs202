1eee3fe396f71edcd92205abd00872b843784d56
uddeepk <uddeepk@gmail.com>
2021-01-26 10:37:35 -0900

    wrote code to observe pointer and array

--- labs/lab03/main.cpp
+++ labs/lab03/main.cpp
@@ -5,6 +5,22 @@
 using std::cout;
 
 int main() {
+    int x;
+    int y;
+    int *xPtr = &x;
+    x = 13;
+    cout << x << "\n";
+    cout << xPtr << "\n";
+    cout << *xPtr << "\n";
+    cout << &y << "\n";
+    ++xPtr;
+    cout << xPtr << "\n";
+    *xPtr = 14;
+    cout << y << "\n";
 
+    int array[10] = { 1, 2, 3, 4};
+    int * aPtr = array;
+    ++aPtr;
+    cout << *aPtr << "\n";
     return 0;
 }
