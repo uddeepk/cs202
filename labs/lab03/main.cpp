87c5685a546218850486658de1afa09b51bf6dec
uddeepk <uddeepk@gmail.com>
2021-01-27 23:25:01 -0900

    transfer ownership to different unique_ptr

--- labs/lab03/main.cpp
+++ labs/lab03/main.cpp
@@ -32,6 +32,7 @@ int main() {
 
     // 1.Create a raw pointer to a dynamically allocated object of your class. Is this object ever
     // destroyed?
+
     cout << "Creating Raw pointer by copying an object!\n ";
     Person per0 = Person("Bart");
     Person * per1 = new Person( per0);
@@ -42,23 +43,33 @@ int main() {
 
     //delete per1;
     per1 = nullptr;
-    // The object is not destroyed. Noticed no destructor called.
+    // ****The object is not destroyed. Noticed no destructor called.
 
     cout << "\n";
+
     // 2. Create a unique_ptr to an object of your class. Note when the object is created, and
     // that it is eventually destroyed. This object should be created with your
     // "other constructor," which takes a parameter.
-    // std::unique_ptr <Person> uPtrPer = std::make_unique<Person> (Person("Andy"));
+
     cout << "Creating unique_ptr to an object of Person class!\n";
-    std::unique_ptr <Person> uPtrPer(new Person("Andy"));
+
+    // std::unique_ptr <Person> uPtrPer = std::make_unique<Person> (Person("Andy"));
     //auto uPtrPer =  std::make_unique<Person> (Person("Andy"));
-    cout << uPtrPer->getName() << endl;
-    cout << endl;
+    std::unique_ptr <Person> uPtrPer(new Person("Andy"));
+
+    cout <<"\n";
     // 3.Transfer ownership of that object to a different unique_ptr. Note that the object itself
     // is not copied.
-    //
+
+    cout << "Transferring ownership of that object to a different unique_ptr\n";
+    auto uPtrPer2 = std::move(uPtrPer);
+
+    // cout << uPtrPer; //points to address of 0 or "nullptr" after std::move
+
+    cout << "\n";
     // 4.Demonstrate the calling of a member function of your object through the unique_ptr.
-    //
+    cout << "Demonstrating the calling of a member function of the object throug unique_ptr!\n";
+
     // 5.Make a shared_ptr to a dynamically allocated object of your class.
     //
     // 6.Make another shared_ptr that points at the same object. Note that the object does not
