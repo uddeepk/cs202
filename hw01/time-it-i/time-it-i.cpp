6aacf18c695cb18942fa4d0875daf868fa4c48df
uddeepk <uddeepk@gmail.com>
2021-01-31 14:03:02 -0900

    added a loop to perform 5 iterations

--- hw01/time-it-i/time-it-i.cpp
+++ hw01/time-it-i/time-it-i.cpp
@@ -25,73 +25,77 @@ int main() {
     // on random integers or doubles created with mt19937 RNG.
     // Measure time for each algorithm atleast five times each for groups of 10 to 10 ^ 9
 
-    //mt19937 random number generator seeded
-    std::mt19937 generator (std::chrono::system_clock::now().time_since_epoch().count());
+    int n = 0;
 
-    std::vector<unsigned int> vecOfNumbers ; //to store all the data
-    size_t numberOfObjects = 10;
+    while ( n < 5) {
+        cout << "Trial " << (n+1) << " \n";
+        //mt19937 random number generator seeded
+        std::mt19937 generator(std::chrono::system_clock::now().time_since_epoch().count());
 
-    for ( int i = 1 ; i <= 9 ; ++i) { // bcuz 10^1 to 10^9
-        cout << "Size " << numberOfObjects << " : \n";
-        //if ( i == 3 )
+        std::vector<unsigned int> vecOfNumbers; //to store all the data
+        size_t numberOfObjects = 10;
+
+        for (int i = 1; i <= 9; ++i) { // bcuz 10^1 to 10^9
+            cout << "Size " << numberOfObjects << " : \n";
+            //if ( i == 3 )
             //break;
-        //Get Vector
-        uniqueStopWatchPtr->Start();
-        getVector(vecOfNumbers, generator, numberOfObjects);
-        uniqueStopWatchPtr->Stop();
-        //Get time for algorithm 5 times ??
+            //Get Vector
+            uniqueStopWatchPtr->Start();
+            getVector(vecOfNumbers, generator, numberOfObjects);
+            uniqueStopWatchPtr->Stop();
+            //Get time for algorithm 5 times ??
 
-        //get needle for search
-        std::vector<unsigned int> needle ;
+            //get needle for search
+            std::vector<unsigned int> needle;
 
-        getNeedle(needle, vecOfNumbers);
+            getNeedle(needle, vecOfNumbers);
 
-        //search needle in hay stack
+            //search needle in hay stack
 
-        uniqueStopWatchPtr->Start();
-        auto it = std::search(vecOfNumbers.begin(), vecOfNumbers.end(), needle.begin(), needle.end());
-        uniqueStopWatchPtr->Stop();
-        cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << "ms ,"
-        << uniqueStopWatchPtr->getCurrentTimeInSeconds() << "s\n";
+            uniqueStopWatchPtr->Start();
+            auto it = std::search(vecOfNumbers.begin(), vecOfNumbers.end(), needle.begin(), needle.end());
+            uniqueStopWatchPtr->Stop();
+            cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << " "
+                 << uniqueStopWatchPtr->getCurrentTimeInSeconds() << "\n";
 
-        //sorting for binary_search and also using it as a run
-        uniqueStopWatchPtr->Start();
-        std::sort(vecOfNumbers.begin(), vecOfNumbers.end());
-        uniqueStopWatchPtr->Stop();
-        cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << "ms ,"
-             << uniqueStopWatchPtr->getCurrentTimeInSeconds() << "s\n";
+            //sorting for binary_search and also using it as a run
+            uniqueStopWatchPtr->Start();
+            std::sort(vecOfNumbers.begin(), vecOfNumbers.end());
+            uniqueStopWatchPtr->Stop();
+            cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << " "
+                 << uniqueStopWatchPtr->getCurrentTimeInSeconds() << "\n";
 
-        //binary_search with sorted vector
-        //get random number, i am using first element of the needle
-        auto myNumberForBinarySearch = *(needle.begin());
+            //binary_search with sorted vector
+            //get random number, i am using first element of the needle
+            auto myNumberForBinarySearch = *(needle.begin());
 
-        uniqueStopWatchPtr->Start();
-        std::binary_search(vecOfNumbers.begin(), vecOfNumbers.end(), myNumberForBinarySearch);
-        uniqueStopWatchPtr->Stop();
-        cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << "ms ,"
-             << uniqueStopWatchPtr->getCurrentTimeInSeconds() << "s\n";
+            uniqueStopWatchPtr->Start();
+            std::binary_search(vecOfNumbers.begin(), vecOfNumbers.end(), myNumberForBinarySearch);
+            uniqueStopWatchPtr->Stop();
+            cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << " "
+                 << uniqueStopWatchPtr->getCurrentTimeInSeconds() << "\n";
 
-        //one more algorithm , shuffle ?? :)
-        std::random_device rd;
-        std::mt19937 myTempGenerator (rd());
+            //one more algorithm , shuffle ?? :)
+            std::random_device rd;
+            std::mt19937 myTempGenerator(rd());
 
-        uniqueStopWatchPtr->Start();
-        std::shuffle(vecOfNumbers.begin(), vecOfNumbers.end(), myTempGenerator);
-        uniqueStopWatchPtr->Stop();
-        cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << "ms ,"
-             << uniqueStopWatchPtr->getCurrentTimeInSeconds() << "s\n";
+            uniqueStopWatchPtr->Start();
+            std::shuffle(vecOfNumbers.begin(), vecOfNumbers.end(), myTempGenerator);
+            uniqueStopWatchPtr->Stop();
+            cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << " "
+                 << uniqueStopWatchPtr->getCurrentTimeInSeconds() << "\n";
 
-        /*if(it != vecOfNumbers.end())
-            cout <<"Successful search\n";
-        else
-            cout <<"Unsuccessful search\n";
-        */
-        //cout << vecOfNumbers.size() << ", " << uniqueStopWatchPtr->getCurrentTimeInSeconds()<< endl;
-        numberOfObjects *= 10;
-        cout << "\n";
+            /*if(it != vecOfNumbers.end())
+                cout <<"Successful search\n";
+            else
+                cout <<"Unsuccessful search\n";
+            */
+            //cout << vecOfNumbers.size() << ", " << uniqueStopWatchPtr->getCurrentTimeInSeconds()<< endl;
+            numberOfObjects *= 10;
+            cout << "\n";
+        }
+    ++n;
     }
-
-
     //std::cout << generator() << std::endl;
     //std::cout <<generator() << std::endl;
     //std::cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << std::endl;
