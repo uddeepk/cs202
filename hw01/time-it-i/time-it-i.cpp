750184a812e939ba19802a5fe749d9337e5485b5
uddeepk <uddeepk@gmail.com>
2021-01-30 21:33:40 -0900

    wrote method to get elements of vectors.

--- hw01/time-it-i/time-it-i.cpp
+++ hw01/time-it-i/time-it-i.cpp
@@ -3,6 +3,13 @@
 #include "StopWatch.hpp"
 #include <memory>
 #include <vector>
+#include <random>
+
+using std::cout;
+using std::endl;
+
+void getVector ( std::vector <unsigned int> &vecOfNumbers, std::mt19937 &myGenerator,
+                 size_t numberOfObjects) ;
 
 int main() {
 
@@ -15,8 +22,32 @@ int main() {
     // on random integers or doubles created with mt19937 RNG.
     // Measure time for each algorithm atleast five times each for groups of 10 to 10 ^ 9
 
-    std::vector<std::vector<double>> vecOfVecOfDbls ; //to store all the data
+    //mt19937 random number generator seeded
+    std::mt19937 generator (std::chrono::system_clock::now().time_since_epoch().count());
 
-    std::cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << std::endl;
+    std::vector<unsigned int> vecOfNumbers ; //to store all the data
+    size_t numberOfObjects = 10;
+
+    for ( int i = 1 ; i <= 9 ; ++i) { // bcuz 10^1 to 10^9
+        //Get Vector
+        uniqueStopWatchPtr->Start();
+        getVector(vecOfNumbers, generator, numberOfObjects);
+        uniqueStopWatchPtr->Stop();
+        //Get time for algorithm 5 times ??
+        cout << vecOfNumbers.size() << ", " << uniqueStopWatchPtr->getCurrentTimeInSeconds()<< endl;
+        numberOfObjects *= 10;
+    }
+
+
+    //std::cout << generator() << std::endl;
+    //std::cout <<generator() << std::endl;
+    //std::cout << uniqueStopWatchPtr->getCurrentTimeInMilliseconds() << std::endl;
     return 0;
+}
+
+void getVector ( std::vector <unsigned int> &vecOfNumbers, std::mt19937 &myGenerator,
+                 size_t numberOfObjects) {
+    for ( auto i = vecOfNumbers.size() ; i < numberOfObjects ; ++i) {
+        vecOfNumbers.push_back(myGenerator());
+    }
 }
