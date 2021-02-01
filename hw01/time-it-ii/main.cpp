2842dfab94de6b8ccc037f82a499af7b734e1701
uddeepk <uddeepk@gmail.com>
2021-01-31 20:18:54 -0900

    commented the plan

--- hw01/time-it-ii/main.cpp
+++ hw01/time-it-ii/main.cpp
@@ -1,4 +1,6 @@
 // Time It II
+// To compile, borrow or link the files from the time-it-i folder.
+
 #include "../time-it-i/StopWatch.hpp"
 #include <iostream>
 #include <fstream>
@@ -8,10 +10,26 @@
 #include <algorithm>
 #include <deque>
 
+/* Read at least 5 Gutenberg Project books with std::string; record the time taken to read with atleast
+ * 3 containers. I am using vector, list, and deque. Also, record times to "find" a random string and
+ * "sort" each container. Graph the results.
+ * Also, talk about memory used. How ????
+ */
 
+// Make a data structure ?? Why ? because a lot of the functions are repeated.
 
 int main() {
     StopWatch myWatch = StopWatch();
+
+    // Read book
+
+    // store random string
+
+    // Find the random string
+
+    // Sort the containers
+
+
     std::cout << "Hello, World!" << std::endl;
     return 0;
 }
