9ae30bc59baf0a7c055172224170d9ccc78b12e1
uddeepk <uddeepk@gmail.com>
2021-01-31 21:24:40 -0900

    wrote struct for Book

--- hw01/time-it-ii/main.cpp
+++ hw01/time-it-ii/main.cpp
@@ -9,6 +9,7 @@
 #include <list>
 #include <algorithm>
 #include <deque>
+#include <memory>
 
 /* Read at least 5 Gutenberg Project books with std::string; record the time taken to read with atleast
  * 3 containers. I am using vector, list, and deque. Also, record times to "find" a random string and
@@ -17,10 +18,44 @@
  */
 
 // Make a data structure ?? Why ? because a lot of the functions are repeated.
+struct Book {
+    // Members Using struct and making all public to easily see use of stopwatch
+    const char * _fileName; //can use this like ifstream.open( _fil..me)
+    std::vector <std::string> bookTextVector;
+    std::list <std::string> bookTextList;
+    std::deque <std::string> bookTextDeque;
 
+    //Constructors
+    Book () = delete ; //disabling the default constructor
+    Book (std::string fileName);
+};
+
+Book::Book ( std::string fileName):_fileName(fileName.c_str()) {
+}
+//Read method
+    //Read all three ways at the same time ??
+    //If read in the same section, the output can be compared for the same book.
+
+//get a random string
+
+// Sort the containers
+
+// function for outputting both seconds and millis ??
+
+static const std::vector <std::string> DEFAULT_FILENAMES = { "pg13332.txt", "22680-0.txt" , "42357-0.txt",
+                                                "pg27507.txt", "pg35461.txt" };
+using std::unique_ptr;
+using std::cout;
 int main() {
     StopWatch myWatch = StopWatch();
-
+    std::ifstream myFile ;
+    //myFile.open("pg13332.txt");
+    std::vector <unique_ptr <Book>> vecOfBookPtrs;
+    // Making vector of books
+    for ( auto path : DEFAULT_FILENAMES) {
+        vecOfBookPtrs.push_back(std::make_unique<Book>(path));
+    }
+    cout << vecOfBookPtrs.size();
     // Read book
 
     // store random string
@@ -30,6 +65,5 @@ int main() {
     // Sort the containers
 
 
-    std::cout << "Hello, World!" << std::endl;
     return 0;
 }
