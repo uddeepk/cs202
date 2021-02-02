97206cf7d9514cb8d7b8c963da4cbd6886936b22
uddeepk <uddeepk@gmail.com>
2021-02-01 21:42:30 -0900

    Initial commit

---
+++ hw01/c_style_strings/c_style_strings.cpp
@@ -0,0 +1,16 @@
+/*
+ * c_style_strings.cpp
+ * Uddeep Karki
+ * Feb 1, 2021
+ *
+ * Make the two following functions without use of any standard library functions.
+ * char* strdup(const char*) //copies C-style string into memroy it allocates dynamically (using new)
+ *
+ * char* findx(const char* s, const har* x) //that finds the first occurance of the C-style string x in s.
+ */
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
