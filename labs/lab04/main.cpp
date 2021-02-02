08f88002e57d95519f5639b24e08e947f9495eef
uddeepk <uddeepk@gmail.com>
2021-02-02 10:15:51 -0900

    added code from Prof's screen

--- labs/lab04/main.cpp
+++ labs/lab04/main.cpp
@@ -2,12 +2,13 @@
 #include <sstream>
 
 using std::cout;
-
+using std::string;
+using std::cin;
 int main() {
+    /*
     std::ostringstream sout;
     sout << "Hello, World!" << 3.14159;
     cout << sout. str() << std::endl;
-
 
     std::istringstream sin("4 25.6 Hello");
     int x;
@@ -17,5 +18,26 @@ int main() {
     sin >> d;
     sin >> s;
 
+    cout <<x << d << s << "\n";
+
+    int i; int j;
+    //std::cin >> i >> j;
+
+    */
+
+    while (true) {
+        cout << "Enter an int and a word: ";
+        string s;
+        getline(cin, s);
+        std::istringstream sin(s);
+        int i;
+        string word;
+        sin >> i >> word;
+        if(!sin) {
+            cout << "You messed up!\n";
+            break;
+        }
+        cout << "int was " << i << " and word was " << word << std::endl;
+    }
     return 0;
 }
