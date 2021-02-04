d5fff1b4410264ac257d2f0ad78344e2d3c18492
uddeepk <uddeepk@gmail.com>
2021-02-03 22:17:06 -0900

    fixed pretty print

--- hw01/time-it-i/TimesForProcessing.cpp
+++ hw01/time-it-i/TimesForProcessing.cpp
@@ -12,7 +12,7 @@ TimesForProcessing::TimesForProcessing(int trialNumber):_trialNumber(trialNumber
 std::ostream& TimesForProcessing::printField (std::ostream &myOutputStream, std::vector<double> &v) {
 
     for ( const auto myData : v) {
-        myOutputStream << std::setw(11) << myData;
+        myOutputStream << std::setw(13) << myData;
     }
     return myOutputStream;
 }
@@ -32,18 +32,18 @@ std::ostream& TimesForProcessing::displayData (std::ostream &os) {
     // Time to Binary
     // Time to Shuffle
     std::vector <int> numberOfObjects = { 10, 100, 1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000};
-    os << "Trial " << _trialNumber << "\n";
-    os << std::setw(12) << "Size";
+    os << "\nTrial " << _trialNumber << "\n";
+    os << std::setw(13) << "Size";
     printField(os, numberOfObjects) << "\n";
-    os << std::setw(12) << "Processing";
+    os << std::setw(13) << "Processing";
     printField (os, _timeForFillingVector) << "\n";
-    os << std::setw(12) << "Search";
+    os << std::setw(13) << "Search";
     printField (os, _timeForSearch) << "\n";
-    os << std::setw(12) << "Sort";
+    os << std::setw(13) << "Sort";
     printField(os, _timeForSort) << "\n";
-    os << std::setw(12) << "BinarySearch";
+    os << std::setw(13) << "BinarySearch";
     printField (os, _timeForBinary_Search) << "\n";
-    os << std::setw(12) << "Shuffle";
+    os << std::setw(13) << "Shuffle";
     printField (os, _timeForShuffle) << "\n";
 
     return os;
