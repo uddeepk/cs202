d09a4eab0487be6bd28092268fb2f7f4c90fbe63
uddeepk <uddeepk@gmail.com>
2021-02-07 18:30:41 -0900

    initial commit 1

---
+++ hw02/linked-list/Dragon.cpp
@@ -0,0 +1,5 @@
+//
+// Created by uddeepk on 2/7/21.
+//
+
+#include "Dragon.hpp"
---
+++ hw02/linked-list/Dragon.hpp
@@ -0,0 +1,16 @@
+//Uddeep Karki
+//Hw02
+//Dragon
+//This is the header for the Dragon class. It has 6? attributes.
+
+
+#ifndef LINKED_LIST_DRAGON_HPP
+#define LINKED_LIST_DRAGON_HPP
+
+
+struct Dragon {
+
+};
+
+
+#endif //LINKED_LIST_DRAGON_HPP
