5af58e9c3dd472e8f73cddcc9cc5e4b9d42dcc0a
uddeepk <uddeepk@gmail.com>
2021-02-07 19:55:13 -0900

    added relational operator less than 1

--- hw02/linked-list/Dragon.cpp
+++ hw02/linked-list/Dragon.cpp
@@ -1,5 +1,41 @@
 //
-// Created by uddeepk on 2/7/21.
+// Uddeep Karki
+// Hw02
+// Dragon.cpp
+// This is the source file for the Dragon header
+// It contains the definitions of the constructors, and member funcitons for the class
 //
 
 #include "Dragon.hpp"
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+Dragon::Dragon (vector <string> &dragonAttributes): _breed(dragonAttributes[0]),
+                _eyeColor(dragonAttributes[1]), _skinColor(dragonAttributes[2]),
+                _nativeLocation(dragonAttributes[3]), _affiliation(dragonAttributes[4]),
+                _length(static_cast<unsigned int>(std::stoi(dragonAttributes[5]))),
+                _name(dragonAttributes[6]) {
+    // Constructor with a vector
+}
+
+Dragon::Dragon ( string breed, string eyeColor, string skinColor, string nativeLocation,
+                 string affiliation, unsigned int length, string name):
+                 _breed(breed), _eyeColor(eyeColor), _skinColor(skinColor),
+                 _nativeLocation(nativeLocation), _affiliation(affiliation),
+                 _length(length), _name(name) {
+    // Constructor with the constructors passed
+}
+
+bool Dragon::operator<(const Dragon &dragon2) {
+    if( _breed < dragon2._breed) {
+        return true;
+    }
+    if (_breed == dragon2._breed && _name < dragon2._name) {
+        return true;
+    }
+
+    return false;
+}
--- hw02/linked-list/Dragon.hpp
+++ hw02/linked-list/Dragon.hpp
@@ -1,6 +1,6 @@
 //Uddeep Karki
 //Hw02
-//Dragon
+//Dragon.hpp
 //This is the header for the Dragon class. It has 6? attributes.
 //The basis of the stats are from the Harry Potter Universe
 
@@ -15,9 +15,11 @@ struct Dragon {
     Dragon () = delete; // disabling default constructor
     Dragon ( std::vector <std::string> &dragonAttributes) ; //constructor with vector
     Dragon ( std::string breed, std::string eyeColor, std::string skinColor,
-    std::string nativeLocation, std::string affiliation, unsigned int length);
-
-
+    std::string nativeLocation, std::string affiliation, unsigned int length, std::string name);
+    Dragon (const Dragon &); // Copy construction
+    ~Dragon();
+    // Overload < operator for using sort on std::list container
+    bool operator<(const Dragon &dragon2);
     //Half dozen fields
     const std::string _breed;
     const std::string _eyeColor;
@@ -25,9 +27,8 @@ struct Dragon {
     const std::string _nativeLocation; //Country it is native in
     const std::string _affiliation; //Preservation areas set for protection and study of dragons
     //Height
-    const unsigned int _length; //Length of an average adult
-
-
+    unsigned int _length; //Length of an average adult
+    const std::string _name;
 };
 
 
