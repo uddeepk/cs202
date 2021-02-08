778db6a470dc179524acc395cb3379c82a079881
uddeepk <uddeepk@gmail.com>
2021-02-07 18:41:43 -0900

    updated Dragon class with 6 fields p1

--- hw02/linked-list/Dragon.hpp
+++ hw02/linked-list/Dragon.hpp
@@ -2,13 +2,24 @@
 //Hw02
 //Dragon
 //This is the header for the Dragon class. It has 6? attributes.
+//The basis of the stats are from the Harry Potter Universe
 
 
 #ifndef LINKED_LIST_DRAGON_HPP
 #define LINKED_LIST_DRAGON_HPP
 
+#include <string>
 
 struct Dragon {
+    //Half dozen fields
+    const std::string breed;
+    const std::string eyeColor;
+    const std::string skinColor;
+    const std::string nativeLocation; //Country it is native in
+    const std::string affiliation; //Preservation areas set for protection and study of dragons
+    //Height
+    const unsigned int length; //Length of an average adult
+
 
 };
 
