faea4321f3ca718457e5e1d628b890b0d7d365fc
uddeepk <uddeepk@gmail.com>
2021-02-08 11:33:05 -0900

    copied the instructions from pdf 2

--- hw02/memory-layout/main.cpp
+++ hw02/memory-layout/main.cpp
@@ -1,5 +1,30 @@
+/*
+ * Uddeep Karki
+ * CS202 HW2
+ * Program 2: Memory Layout
+ *
+ * This C++ prgoram tells the order in which static storage, the stack, and the free store are
+ * laid out in memory.
+ * Which direction does the stack grow? upward toward higher addresses or downward towards
+ * lower addresses?
+ * In an array on the free store, are elements with higher indices allocated at higher or lower
+ * addresses?
+ *
+ * Code
+ * Static data
+ * Free store
+ * Stack
+ */
+
+
 #include <iostream>
 
+void printPointer(int i) {
+    int* iptr = &i;
+    // the unary * operator dereferences the pointer
+    std::cout << "i = " << *iptr << "\n";
+    std::cout << "&i = " << iptr << "\n";
+}
 int main() {
     std::cout << "Hello, World!" << std::endl;
     return 0;
