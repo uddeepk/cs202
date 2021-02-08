b4690f6a7102799ec8acd619708339b78273e14c
uddeepk <uddeepk@gmail.com>
2021-02-07 19:02:21 -0900

    updated header and field names

--- hw02/linked-list/Dragon.hpp
+++ hw02/linked-list/Dragon.hpp
@@ -9,16 +9,23 @@
 #define LINKED_LIST_DRAGON_HPP
 
 #include <string>
+#include <vector>
 
 struct Dragon {
+    Dragon () = delete; // disabling default constructor
+    Dragon ( std::vector <std::string> &dragonAttributes) ; //constructor with vector
+    Dragon ( std::string breed, std::string eyeColor, std::string skinColor,
+    std::string nativeLocation, std::string affiliation, unsigned int length);
+
+
     //Half dozen fields
-    const std::string breed;
-    const std::string eyeColor;
-    const std::string skinColor;
-    const std::string nativeLocation; //Country it is native in
-    const std::string affiliation; //Preservation areas set for protection and study of dragons
+    const std::string _breed;
+    const std::string _eyeColor;
+    const std::string _skinColor;
+    const std::string _nativeLocation; //Country it is native in
+    const std::string _affiliation; //Preservation areas set for protection and study of dragons
     //Height
-    const unsigned int length; //Length of an average adult
+    const unsigned int _length; //Length of an average adult
 
 
 };
