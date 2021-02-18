2fefcd998fb734d351abe8a82cdd230640286411
uddeepk <uddeepk@gmail.com>
2021-02-17 22:40:43 -0900

    add datamembers and functions

--- hw03/a-simple-tokenizer/TokenAndPosition.hpp
+++ hw03/a-simple-tokenizer/TokenAndPosition.hpp
@@ -5,10 +5,19 @@
 #ifndef A_SIMPLE_TOKENIZER_TOKENANDPOSITION_HPP
 #define A_SIMPLE_TOKENIZER_TOKENANDPOSITION_HPP
 
+#include <string>
+#include <vector>
+#include <iostream>
 
-class TokenAndPosition {
-
+struct TokenAndPosition {
+    std::string _token;
+    int _line;
+    unsigned int _column;
 };
+
+std::vector <std::string> lineToTokens (const std::string &line);
+std::vector <TokenAndPosition> readLines(std::istream &is);
+void printTokens(std::ostream &os, const std::vector<TokenAndPosition> &tokens);
 
 
 #endif //A_SIMPLE_TOKENIZER_TOKENANDPOSITION_HPP
