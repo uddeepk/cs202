00e1e52d3223b05940a7876f3724f1ed10b88d58
uddeepk <uddeepk@gmail.com>
2021-03-03 22:53:11 -0900

    wrote test for smart array init

--- labs/smart-array/tests.cpp
+++ labs/smart-array/tests.cpp
@@ -6,7 +6,11 @@
 #include "catch.hpp"
 
 TEST_CASE("Check smart array intitialized") {
-SmartArray sa(10);
-sa[0] = 13;
-//REQUIRE()
+//SmartArray sa(10);
+//sa[0] = 13;
+//REQUIRE(sa.size() == 10);
+//REQUIRE(sa[0] ==13);
+//const SmartArray csa(sa);
+//REQUIRE(csa[0] == 13);
+
 }
